fc3ea3320fd346898555e28cdcbd7c900e9c4f7e
Martin Jaros <jaros@arion.cz>
2019-12-08 08:57:24 +0100

    D8

---
+++ D8a/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCount = 6, colCount = 25, layerCount = 0, zeroCount = int.MaxValue, oneCount = 0, twoCount = 0;
+            int[][][] image = null;
+            
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data 2019\\D8\\input.txt"))
+            {
+                string img = "", line = "";
+                while ((line = input.ReadLine()) != null)
+                {
+                    img += line;
+                }
+
+                layerCount = img.Length / rowCount / colCount;
+                image = new int[layerCount][][];
+                int index = 0;
+                for (int i = 0; i < layerCount; i++)
+                {
+                    image[i] = new int[rowCount][];
+                    for (int j = 0; j < rowCount; j++)
+                    {
+                        image[i][j] = new int[colCount];
+                        for (int k = 0; k < colCount; k++)
+                        {
+                            image[i][j][k] = Convert.ToInt32(img[index].ToString());
+                            index++;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < layerCount; i++)
+            {
+                int tempZero = 0, tempOne = 0, tempTwo = 0;
+                for (int j = 0; j < rowCount; j++)
+                {
+                    tempZero += image[i][j].Count(a => a == 0);
+                    tempOne += image[i][j].Count(a => a == 1);
+                    tempTwo += image[i][j].Count(a => a == 2);
+                }
+                if (tempZero < zeroCount)
+                {
+                    zeroCount = tempZero;
+                    oneCount = tempOne;
+                    twoCount = tempTwo;
+                }
+            }
+
+            Console.WriteLine((oneCount * twoCount).ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ D8b/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D8b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rowCount = 6, colCount = 25, layerCount = 0;
+            int[][][] image = null;
+
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data 2019\\D8\\input.txt"))
+            {
+                string img = "", line = "";
+                while ((line = input.ReadLine()) != null)
+                {
+                    img += line;
+                }
+
+                layerCount = img.Length / rowCount / colCount;
+                image = new int[layerCount][][];
+                int index = 0;
+                for (int i = 0; i < layerCount; i++)
+                {
+                    image[i] = new int[rowCount][];
+                    for (int j = 0; j < rowCount; j++)
+                    {
+                        image[i][j] = new int[colCount];
+                        for (int k = 0; k < colCount; k++)
+                        {
+                            image[i][j][k] = Convert.ToInt32(img[index].ToString());
+                            index++;
+                        }
+                    }
+                }
+            }
+
+            for (int i = 1; i < layerCount; i++)
+            {
+                for (int j = 0; j < rowCount; j++)
+                {
+                    for (int k = 0; k < colCount; k++)
+                    {
+                        if (image[0][j][k] == 2)
+                            image[0][j][k] = image[i][j][k];
+                    }
+                }
+            }
+            
+            for (int j = 0; j < rowCount; j++)
+            {
+                for (int k = 0; k < colCount; k++)
+                {
+                    Console.Write(image[0][j][k] == 1 ? "1" : " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
