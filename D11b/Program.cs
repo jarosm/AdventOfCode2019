9efe3faca3894bf753ac846a85d77b633060101c
Martin Jaros <jaros@arion.cz>
2019-12-11 20:57:07 +0100

    D11

---
+++ D11b/Program.cs
@@ -0,0 +1,479 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D11b
+{
+    class Program
+    {
+        static string prog = "";
+        static Int64[] steps = null;
+        static Int64 relativeBase = 0;
+
+        static Int64[] GetIntArray(string prog)
+        {
+            string[] steps = prog.Split(',');
+            List<Int64> listOfInts = new List<Int64>();
+            for (int i = 0; i < steps.Length; i++)
+            {
+                listOfInts.Add(Convert.ToInt64(steps[i]));
+            }
+            // memory buffer
+            for (int i = 0; i < 1000; i++)
+            {
+                listOfInts.Add(0);
+            }
+            return listOfInts.ToArray();
+        }
+
+        static void IntProgram(Int64 inVal, ref Int64 outVal, ref Int64 index, ref int statusCode)
+        {
+            try
+            {
+                bool end = false;
+
+                while (!end && (index < steps.Length))
+                {
+                    Int64 first = 0, second = 0, third = 0, temp = 0;
+
+                    switch (steps[index] % 100)
+                    {
+                        case 1: // +
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    third = index + 3;
+                                    break;
+                                case 2:
+                                    third = relativeBase + steps[index + 3];
+                                    break;
+                                default:
+                                    third = steps[index + 3];
+                                    break;
+                            }
+
+                            steps[third] = steps[first] + steps[second];
+
+                            index += 4;
+                            break;
+
+                        case 2: // *
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    third = index + 3;
+                                    break;
+                                case 2:
+                                    third = relativeBase + steps[index + 3];
+                                    break;
+                                default:
+                                    third = steps[index + 3];
+                                    break;
+                            }
+
+                            steps[third] = steps[first] * steps[second];
+
+                            index += 4;
+                            break;
+
+                        case 3: // input
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            steps[first] = inVal;
+
+                            index += 2;
+                            break;
+
+                        case 4: // output
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            outVal = steps[first];
+
+                            index += 2;
+
+                            end = true;
+                            statusCode = 0;
+                            break;
+
+                        case 5: // jnz
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+
+                            if (steps[first] != 0)
+                                index = steps[second];
+                            else
+                                index += 3;
+                            break;
+
+                        case 6: // jz
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+
+                            if (steps[first] == 0)
+                                index = steps[second];
+                            else
+                                index += 3;
+                            break;
+
+                        case 7: // <
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    third = index + 3;
+                                    break;
+                                case 2:
+                                    third = relativeBase + steps[index + 3];
+                                    break;
+                                default:
+                                    third = steps[index + 3];
+                                    break;
+                            }
+
+                            if (steps[first] < steps[second])
+                                steps[third] = 1;
+                            else
+                                steps[third] = 0;
+
+                            index += 4;
+                            break;
+
+                        case 8: // ==
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    second = index + 2;
+                                    break;
+                                case 2:
+                                    second = relativeBase + steps[index + 2];
+                                    break;
+                                default:
+                                    second = steps[index + 2];
+                                    break;
+                            }
+                            temp = temp / 10;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    third = index + 3;
+                                    break;
+                                case 2:
+                                    third = relativeBase + steps[index + 3];
+                                    break;
+                                default:
+                                    third = steps[index + 3];
+                                    break;
+                            }
+
+                            if (steps[first] == steps[second])
+                                steps[third] = 1;
+                            else
+                                steps[third] = 0;
+
+                            index += 4;
+                            break;
+
+                        case 9: // adjust relative index
+                            temp = steps[index] / 100;
+                            switch (temp % 10)
+                            {
+                                case 1:
+                                    first = index + 1;
+                                    break;
+                                case 2:
+                                    first = relativeBase + steps[index + 1];
+                                    break;
+                                default:
+                                    first = steps[index + 1];
+                                    break;
+                            }
+
+                            relativeBase += steps[first];
+
+                            index += 2;
+                            break;
+
+                        case 99:
+                            end = true;
+                            statusCode = 1;
+                            index++;
+                            break;
+
+                        default:
+                            end = true;
+                            statusCode = 1;
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        static void Main(string[] args)
+        {
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data 2019\\D11\\input.txt"))
+            {
+                string line = "";
+                while ((line = input.ReadLine()) != null)
+                {
+                    prog += line;
+                }
+            }
+
+            steps = GetIntArray(prog);
+
+            Int64 paint = 0, rotation = 0, index = 0;
+            int status = 0;
+            int direction = 0, x = 10, y = 20, maxX = 60, maxY = 30;
+            (bool painted, Int64 color)[,] hull = new (bool painted, Int64 color)[maxX, maxY];
+
+            hull[x, y].color = 1;
+
+            while (status != 1)
+            {
+                IntProgram(hull[x, y].color, ref paint, ref index, ref status);
+                if (status == 1)
+                    break;
+
+                hull[x, y].color = paint;
+                hull[x, y].painted = true;
+
+                IntProgram(hull[x, y].color, ref rotation, ref index, ref status);
+                if (status == 1)
+                    break;
+
+                switch (direction)
+                {
+                    case 0: // up
+                        if (rotation == 0)
+                        {
+                            direction = 3;
+                            x--;
+                        }
+                        else
+                        {
+                            direction = 1;
+                            x++;
+                        }
+                        break;
+                    case 1: // right
+                        if (rotation == 0)
+                        {
+                            direction = 0;
+                            y++;
+                        }
+                        else
+                        {
+                            direction = 2;
+                            y--;
+                        }
+                        break;
+                    case 2: // down
+                        if (rotation == 0)
+                        {
+                            direction = 1;
+                            x++;
+                        }
+                        else
+                        {
+                            direction = 3;
+                            x--;
+                        }
+                        break;
+                    case 3: // left
+                        if (rotation == 0)
+                        {
+                            direction = 2;
+                            y--;
+                        }
+                        else
+                        {
+                            direction = 0;
+                            y++;
+                        }
+                        break;
+                }
+            }
+
+
+            for (int j = 0; j < maxY; j++)
+            {
+                for (int i = 0; i < maxX; i++)
+                {
+                    Console.Write(hull[i, j].color == 1 ? "X" : " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
