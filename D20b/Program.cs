82ebf7d495b545ef9ef3825f24c16faed319c9c3
Martin Jaros <jaros@arion.cz>
2019-12-20 15:57:08 +0100

    D20

---
+++ D20b/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D20b
+{
+    class Program
+    {
+        const int start = 'A' * 100 + 'A';
+        const int end = 'Z' * 100 + 'Z';
+        static P startPosition;
+        static P[][] map;
+
+        private struct P : IEquatable<P>
+        {
+            public bool Passable { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
+            public int LevelDirection { get; set; }
+            public int Level { get; set; }
+            public int Portal { get; set; }
+            public int OppositePortalX { get; set; }
+            public int OppositePortalY { get; set; }
+
+            public P(P from, int level)
+            {
+                Passable = from.Passable;
+                X = from.X;
+                Y = from.Y;
+                LevelDirection = from.LevelDirection;
+                Level = level;
+                Portal = from.Portal;
+                OppositePortalX = from.OppositePortalX;
+                OppositePortalY = from.OppositePortalY;
+            }
+
+            public bool Equals(P other)
+            {
+                if (ReferenceEquals(null, other)) return false;
+                if (ReferenceEquals(this, other)) return true;
+                return X == other.X && Y == other.Y && Level == other.Level;
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj)) return false;
+                if (ReferenceEquals(this, obj)) return true;
+                if (obj.GetType() != this.GetType()) return false;
+                return Equals((P)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked // Overflow is fine, just wrap
+                {
+                    int hash = 17;
+                    // Suitable nullity checks etc, of course :)
+                    hash = hash * 23 + X.GetHashCode();
+                    hash = hash * 23 + Y.GetHashCode();
+                    hash = hash * 23 + Level.GetHashCode();
+                    return hash;
+                }
+            }
+        }
+
+        static int GetDistance()
+        {
+            var visited = new HashSet<P>();
+            var q = new Queue<P>();
+            var s = new Queue<int>();
+            q.Enqueue(startPosition);
+            s.Enqueue(0);
+
+            while (q.Any())
+            {
+                var pos = q.Dequeue();
+                var dist = s.Dequeue();
+
+                if (pos.Portal == end && pos.Level == 0)
+                    return dist;
+                if (visited.Contains(pos))
+                    continue;
+
+                visited.Add(pos);
+
+                if (pos.Portal > 0 && pos.Portal != start && pos.Portal != end && !(pos.Level == 0 && pos.LevelDirection == -1))
+                {
+                    q.Enqueue(new P(map[pos.OppositePortalY][pos.OppositePortalX], pos.Level + pos.LevelDirection));
+                    s.Enqueue(dist + 1);
+                }
+                for (int i = 0; i < 4; i++)
+                {
+                    int x2 = -1, y2 = -1;
+                    switch (i)
+                    {
+                        case 0:
+                            x2 = pos.X;
+                            y2 = pos.Y - 1;
+                            break;
+                        case 1:
+                            x2 = pos.X + 1;
+                            y2 = pos.Y;
+                            break;
+                        case 2:
+                            x2 = pos.X;
+                            y2 = pos.Y + 1;
+                            break;
+                        case 3:
+                            x2 = pos.X - 1;
+                            y2 = pos.Y;
+                            break;
+                    }
+                    if (x2 >= 0 && x2 < map[0].Length && y2 >= 0 && y2 < map.Length && map[y2][x2].Passable)
+                    {
+                        q.Enqueue(new P(map[y2][x2], pos.Level));
+                        s.Enqueue(dist + 1);
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data 2019\\D20\\input.txt"))
+            {
+                string line = "";
+                List<string> lines = new List<string>();
+                while ((line = input.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                // create map
+                int maxX = lines[0].Length, maxY = lines.Count;
+                map = new P[maxY - 4][];
+                for (int y = 2; y < maxY - 2; y++)
+                {
+                    map[y - 2] = new P[maxX - 4];
+                    for (int x = 2; x < maxX - 2; x++)
+                    {
+                        if (lines[y][x] == '.')
+                        {
+                            int p = 0, levelDirection = 0;
+
+                            if (lines[y - 1][x] >= 'A' && lines[y - 1][x] <= 'Z')
+                                p = lines[y - 2][x] * 100 + lines[y - 1][x];
+                            else if (lines[y + 1][x] >= 'A' && lines[y + 1][x] <= 'Z')
+                                p = lines[y + 1][x] * 100 + lines[y + 2][x];
+                            else if (lines[y][x - 1] >= 'A' && lines[y][x - 1] <= 'Z')
+                                p = lines[y][x - 2] * 100 + lines[y][x - 1];
+                            else if (lines[y][x + 1] >= 'A' && lines[y][x + 1] <= 'Z')
+                                p = lines[y][x + 1] * 100 + lines[y][x + 2];
+
+                            if (p > 0)
+                            {
+                                if (p == start || p == end)
+                                    levelDirection = 0;
+                                else
+                                {
+                                    if (x == 2 || x == maxX - 3 || y == 2 || y == maxY - 3)
+                                        levelDirection = -1;
+                                    else
+                                        levelDirection = 1;
+                                }
+                            }
+
+                            map[y - 2][ x - 2] = new P() { Passable = true, X = x - 2, Y = y - 2, Portal = p, OppositePortalX = -1, OppositePortalY = -1, LevelDirection = levelDirection, Level = 0 };
+                        }
+                        else
+                            map[y - 2][x - 2] = new P() { Passable = false, X = x - 2, Y = y - 2, Portal = 0, OppositePortalX = -1, OppositePortalY = -1, LevelDirection = 0, Level = 0 };
+                    }
+                }
+
+                // find oposite portals
+                for (int y = 0; y < map.Length; y++)
+                {
+                    for (int x = 0; x < map[y].Length; x++)
+                    {
+                        if (map[y][x].Portal == start)
+                            startPosition = map[y][x];
+
+                        if (map[y][x].Passable && map[y][x].Portal > 0 && map[y][x].Portal != start && map[y][x].Portal != end && map[y][x].OppositePortalX == -1)
+                        {
+                            int ox = x + 1, oy = y;
+                            while (true)
+                            {
+                                if (ox >= map[y].Length)
+                                {
+                                    ox = 0;
+                                    oy++;
+                                }
+                                if (map[oy][ox].Portal == map[y][x].Portal)
+                                {
+                                    map[oy][ox].OppositePortalX = x;
+                                    map[oy][ox].OppositePortalY = y;
+                                    map[y][x].OppositePortalX = ox;
+                                    map[y][x].OppositePortalY = oy;
+                                    break;
+                                }
+                                ox++;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(GetDistance());
+            Console.WriteLine("end");
+            Console.ReadLine();
+        }
+    }
+}
